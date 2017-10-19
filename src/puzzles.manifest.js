2397c02a28307b357339e4350288a01464c5f1fb
mstop4 <mstop4@gmail.com>
2017-10-19 16:20:45 -0400

    fixed some errors

--- src/puzzles.manifest.js
+++ src/puzzles.manifest.js
@@ -2,15 +2,15 @@ export const puzzles = [
   {
     id: 0,
     name: "Squirrel",
-    file: "videos/squirrel.mp4",
-    preview: "https://s3.us-east-2.amazonaws.com/videopuzzle/Puzzles/squirrel.mp4",
+    file: "https://s3.us-east-2.amazonaws.com/videopuzzle/Puzzles/squirrel.mp4",
+    preview: "vid_squirrelPreview",
     scale: 2
   },
 
   {
     id: 1,
     name: "Shore",
-    file: "http://s3.us-east-2.amazonaws.com/videopuzzle/shore.mp4",
+    file: "http://s3.us-east-2.amazonaws.com/videopuzzle/Puzzles/shore.mp4",
     preview: "vid_shorePreview",
     scale: 2
   },
